bcbf8a42d02cd49a9860af63df5745591e27fa88
Alexpl0 <j.alejandro.pl@gmail.com>
2025-03-05 14:07:24 -0600

    Metodo PUT DataTables

--- assets/js/DataTables.js
+++ assets/js/DataTables.js
@@ -2,12 +2,12 @@ let dataTable;
 let dataTableIsInitialized = false;
 
 const dataTableOptions = {
-        lengthMenu: [5, 10, 15, 20, 100, 200, 500],
+    lengthMenu: [5, 10, 15, 20, 100, 200, 500],
     columnDefs: [
         { className: "centered", targets: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] },
         { orderable: false, targets: [9, 10] },
         { searchable: false, targets: [1] }
-            ],
+    ],
     pageLength: 3,
     destroy: true,
     language: {
@@ -31,102 +31,178 @@ const tableBody_users = document.getElementById('tableBody_users');
 
 const inventariocrud = async () => {
     try {
-        const inventarioResponse = await fetch("http://localhost:8080/inventario");
+        const inventarioResponse = await fetch("http://localhost:8080/productos");
         const inventario = await inventarioResponse.json();
-    let content = ``;
-    inventario.forEach((inventario, index) => {
-        content += `
-            <tr>
-                <td>${index + 1}</td>
-                <td>${inventario.nombre}</td>
-                <td>${inventario.marca}</td>
-                <td>${inventario.modelo}</td>
-                <td>${inventario.estado}</td>
-                <td>${inventario.descripcion}</td>
-                <td>${inventario.precio}</td>
-                <td>${inventario.categoria}</td>
-                <td>${inventario.ubicacion}</td>
-                <td><i class="fa-solid fa-check" style="color: green;"></i></td>
-                <td>
-                    <button class="btn btn-sm btn-primary"><i class="fa-solid fa-pencil"></i></button>
-                    <button class="btn btn-sm btn-danger"><i class="fa-solid fa-trash-can"></i></button>
-                </td>
-            </tr>`;
-    });
-    tableBody_users.innerHTML = content;
+
+        let content = ``;
+        inventario.forEach((inventario, index) => {
+            content += `
+                <tr>
+                    <td>${inventario.id}</td>
+                    <td>${inventario.nombre}</td>
+                    <td>${inventario.marca}</td>
+                    <td>${inventario.modelo}</td>
+                    <td>${inventario.estado}</td>
+                    <td>${inventario.descripcion}</td>
+                    <td>${inventario.precio}</td>
+                    <td>${inventario.categoria.nombre}</td> 
+                    <td>${inventario.ubicacion.nombre}</td>
+                    <td><i class="fa-solid fa-check" style="color: green;"></i></td>
+                    <td>
+                        <button id="editbtn" class="btn btn-sm btn-primary" data-id="${inventario.id}"><i class="fa-solid fa-pencil"></i></button>
+                        <button id="deletebtn" class="btn btn-sm btn-danger" data-id="${inventario.id}"><i class="fa-solid fa-trash-can"></i></button>
+                    </td>
+                </tr>`;
+        });
+        tableBody_users.innerHTML = content;
     } catch (ex) {
         alert(ex);
     }
 };
 
 const initDataTable = async () => {
+
+    
+
     if (dataTableIsInitialized) {
         dataTable.destroy();
     }
 
     await inventariocrud();
 
+
     dataTable = $("#datatable_users").DataTable(dataTableOptions);
 
     dataTableIsInitialized = true;
 };
 
+document.addEventListener('click', async (event) => {
+    if (event.target && event.target.id === 'deletebtn') {
+        const id = event.target.getAttribute('data-id');
+        const confirmed = confirm('¿Estás seguro de que deseas eliminar este elemento?');
+        if (confirmed) {
+            try {
+                const response = await fetch(`http://localhost:8080/productos/${id}`, {
+                    method: 'DELETE'
+                });
+                if (response.ok) {
+                    alert('Elemento eliminado con éxito');
+                    await initDataTable(); // Recargar la tabla después de la eliminación
+                } else {
+                    alert('Error al eliminar el elemento');
+                }
+            } catch (error) {
+                alert('Error al eliminar el elemento despues del if');
+            }
+        }
+    }
+
+    if (event.target && event.target.id === 'editbtn') {
+        const id = event.target.getAttribute('data-id');
+        const inventarioResponse = await fetch(`http://localhost:8080/productos/${id}`);
+        const inventario = await inventarioResponse.json();
+
+        // Mostrar un formulario de edición con los datos del inventario
+        const editForm = `
+            <div id="form" class="contenedor">
+                <form action="" id="formulario" class="formulario">
+                    <label for="unique-id">Identificador Único:</label>
+                    <p id="unique-id">${id}</p><br><br>
+
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" id="nombre" name="nombre" value="${inventario.nombre}" required><br><br>
+
+                    <label for="marca">Marca:</label>
+                    <input type="text" id="marca" name="marca" value="${inventario.marca}" required><br><br>
+
+                    <label for="modelo">Modelo:</label>
+                    <input type="text" id="modelo" name="modelo" value="${inventario.modelo}" required><br><br>
+
+                    <label for="estado">Estado:</label>
+                    <select id="estado" name="estado" required>
+                        <option value="Nuevo" ${inventario.estado === 'Nuevo' ? 'selected' : ''}>Nuevo</option>
+                        <option value="Como Nuevo" ${inventario.estado === 'Como Nuevo' ? 'selected' : ''}>Como Nuevo</option>
+                        <option value="Medio" ${inventario.estado === 'Medio' ? 'selected' : ''}>Medio</option>
+                        <option value="Malo" ${inventario.estado === 'Malo' ? 'selected' : ''}>Malo</option>
+                    </select><br><br>
+
+                    <label for="descripcion">Descripción detallada:</label>
+                    <textarea id="descripcion" name="descripcion" required>${inventario.descripcion}</textarea><br><br>
+
+                    <label for="precio">Precio:</label>
+                    <input type="text" id="precio" name="precio" value="${inventario.precio}" required><br><br>
+
+                    <label for="categoria">Categoría:</label>
+                    <select id="categoria" name="categoria" required>
+                        <option value="1" ${inventario.categoria.id === 1 ? 'selected' : ''}>Monitor</option>
+                        <option value="2" ${inventario.categoria.id === 2 ? 'selected' : ''}>Teclado</option>
+                        <option value="3" ${inventario.categoria.id === 3 ? 'selected' : ''}>Mouse</option>
+                        <option value="4" ${inventario.categoria.id === 4 ? 'selected' : ''}>CPU</option>
+                        <option value="5" ${inventario.categoria.id === 5 ? 'selected' : ''}>NoBreak</option>
+                    </select><br><br>
+                    
+                    <label for="ubicacion">Ubicación:</label>
+                    <select id="ubicacion" name="ubicacion" required>
+                        <option value="1" ${inventario.ubicacion.id === 1 ? 'selected' : ''}>Sala 1</option>
+                        <option value="2" ${inventario.ubicacion.id === 2 ? 'selected' : ''}>Sala 2</option>
+                    </select><br><br>
+
+                    <button id="actual" type="submit">Actualizar</button>
+                </form>
+            </div>
+        `;
+
+        // Insert the edit form HTML into the container with id 'editFormContainer'
+        $('#editFormContainer').html(editForm);
+
+        // Add an event listener to the button with id 'actual' to handle the form submission
+        $('#actual').on('click', async function (e) {
+            e.preventDefault(); // Prevent the default form submission behavior
+
+            // Create an object 'updatedInventario' with the updated values from the form inputs
+            const updatedInventario = {
+                nombre: $('#nombre').val(), // Get the value of the input with id 'nombre'
+                marca: $('#marca').val(), // Get the value of the input with id 'marca'
+                modelo: $('#modelo').val(), // Get the value of the input with id 'modelo'
+                estado: $('#estado').val(), // Get the value of the select with id 'estado'
+                descripcion: $('#descripcion').val(), // Get the value of the textarea with id 'descripcion'
+                precio: $('#precio').val(), // Get the value of the input with id 'precio'
+                ubicacion: { id: ($('#ubicacion').val()) }, // Get the value of the select with id 'ubicacion' and convert it to BigInt
+                categoria: { id: ($('#categoria').val()) }, // Get the value of the select with id 'categoria' and convert it to BigInt
+            };
+
+            const json = JSON.stringify(updatedInventario);
+            console.log(json);
+
+            try {
+                // Send a PUT request to update the inventory item with the specified id
+                const response = await fetch(`http://localhost:8080/productos/${id}`, {
+                    method: 'PUT', // Use the PUT method to update the resource
+                    headers: {
+                        'Content-Type': 'application/json' // Set the content type to JSON
+                    },
+                    body: JSON.stringify(updatedInventario) // Convert the 'updatedInventario' object to a JSON string
+                });
+
+                console.log(updatedInventario);
+
+                // Check if the response is OK (status code 200-299)
+                if (response.ok) {
+                    $('#formulario').css('display', 'none'); // Hide the edit form container
+                    document.getElementById('form').style.display = 'none';
+                    alert('Elemento actualizado con éxito'); // Show a success message
+                    await initDataTable(); // Reload the data table to reflect the changes
+                    
+                } else {
+                    alert('Error al actualizar el elemento'); // Show an error message if the response is not OK
+                }
+            } catch (error) {
+                //alert('Error al actualizar el elemento Catch'); // Show an error message if there is an exception
+            }
+        });
+    }
+});
+
 window.addEventListener("load", async () => {
     await initDataTable();
 });
-
-
-
-
-/*
-const listUsersAndCategoriesAndUbication = async () => {
-    try {
-        const [usersResponse, categoriesResponse, ubicacionesResponse] = await Promise.all([
-            fetch("http://localhost:8080/productos"),
-            fetch("http://localhost:8080/categorias"),
-            fetch("http://localhost:8080/ubicaciones")
-        ]);
-
-        const users = await usersResponse.json();
-        const categories = await categoriesResponse.json();
-        const ubicacion = await ubicacionesResponse.json();
-
-        let categoria = "";
-        if (categories.index == "1") {
-            categoria = "Monitor";
-        }
-
-        let ubi = "";
-        if (ubicacion.id == "") {
-            ubi = "Sala1";
-        }
-
-        let content = ``;
-        users.forEach((user, index) => {
-            content += `
-                <tr>
-                    <td>${index + 1}</td>
-                    <td>${user.nombre}</td>
-                    <td>${user.marca}</td>
-                    <td>${user.modelo}</td>
-                    <td>${user.estado}</td>
-                    <td>${user.descripcion}</td>
-                    <td>${user.precio}</td>
-                    <td>${categoria}</td>
-                    <td>${ubi}</td>
-                    <td><i class="fa-solid fa-check" style="color: green;"></i></td>
-                    <td>
-                        <button class="btn btn-sm btn-primary"><i class="fa-solid fa-pencil"></i></button>
-                        <button class="btn btn-sm btn-danger"><i class="fa-solid fa-trash-can"></i></button>
-                    </td>
-                </tr>`;
-        });
-
-        // You can also process categories here if needed
-
-        tableBody_users.innerHTML = content;
-    } catch (ex) {
-        alert(ex);
-    }
-};
-*/
--- assets/js/newQR.js
+++ assets/js/newQR.js
@@ -19,13 +19,13 @@ $(document).ready(function () {
         // Crea un nuevo código QR en el contenedor con el texto ingresado
         var qrcode = new QRCode(document.getElementById('qrcode-container'), {
             text: text,
-            width: 96, 
+            width: 96,
             height: 96
         });
 
         // Crea una nueva imagen para el logo
         var logo = new Image();
-        logo.src= "../media/images.jpg"; // Actualiza la ruta a tu logo.png
+        logo.src = "../media/images.jpg"; // Actualiza la ruta a tu logo.png
 
         // Espera a que el logo se cargue, luego lo superpone en el código QR
         logo.onload = function () {
@@ -56,8 +56,6 @@ $(document).ready(function () {
 
             // Muestra el botón de descarga
             $('#download-button').show();
-
-            
         };
     });
 
@@ -80,4 +78,126 @@ $(document).ready(function () {
             // Simula un clic en el enlace para iniciar la descarga
         });
     });
+
+    // Event listener para el botón "Ingresar Nuevo Producto"
+    $('#new-product-button').on('click', async function () {
+        try {
+            // Realiza el POST inicial para crear una nueva entrada sin campos
+            const response = await fetch('http://localhost:8080/inventario', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({})
+            });
+
+            if (response.ok) {
+                // Obtiene la nueva entrada creada
+                const newEntry = await response.json();
+                const newId = newEntry.id;
+
+                // Muestra el formulario de edición con el ID obtenido
+                const formHtml = `
+                    <div class="contenedor">
+                        <form action="" id="formulario" class="formulario">
+                            <label for="unique-id">Identificador Único:</label>
+                            <p id="unique-id">${newId}</p><br><br>
+
+                            <label for="categoria">Categoría:</label>
+                            <select id="categoria" name="categoria" required>
+                                <option value="1">Monitor</option>
+                                <option value="2">Teclado</option>
+                                <option value="3">Mouse</option>
+                                <option value="4">CPU</option>
+                                <option value="5">NoBreak</option>
+                            </select><br><br>
+                            
+                            <label for="descripcion">Descripción detallada:</label>
+                            <textarea id="descripcion" name="descripcion" required></textarea><br><br>
+
+                            <label for="estado">Estado:</label>
+                            <input type="text" id="estado" name="estado" required><br><br>
+
+                            <label for="marca">Marca:</label>
+                            <input type="text" id="marca" name="marca" required><br><br>
+
+                            <label for="modelo">Modelo:</label>
+                            <input type="text" id="modelo" name="modelo" required><br><br>
+
+                            <label for="price">Nombre:</label>
+                            <input type="text" id="nombre" name="nombre" required><br><br>
+
+                            <label for="price">Precio:</label>
+                            <input type="text" id="precio" name="precio" required><br><br>
+
+                            <label for="ubicacion">Ubicación:</label>
+                            <input type="text" id="ubicacion" name="ubicacion" required><br><br>
+                            
+
+                            <button type="submit" class="btn btn-primary" id="generate-button">
+                                <i class="fas fa-qrcode"></i> Generar QR
+                            </button>
+                        </form>
+
+                        <div id="contenedorQR" class="contenedorQR"></div>
+                    
+                        <!-- Aqui es el Contenedor del QR -->
+                        <div class="row justify-content-center mt-4">
+                            <div class="col-md-6 text-center">
+                                <div id="qrcode-container"></div>
+                            </div>
+                        </div>
+                        <!-- Aqui es el boton de descargar -->
+                        <div class="row justify-content-center mt-2">
+                            <div class="col-md-6 text-center">
+                                <button id="download-button" class="btn btn-success">
+                                    <i class="fas fa-download"></i> Descargar QR
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                $('#form-container').html(formHtml);
+
+                // Añadir event listener para el formulario de edición
+                $('#formulario').on('submit', async function (e) {
+                    e.preventDefault();
+                    const updatedInventario = {
+                        nombre: $('#nombre').val(),
+                        marca: $('#marca').val(),
+                        modelo: $('#modelo').val(),
+                        estado: $('#estado').val(),
+                        descripcion: $('#descripcion').val(),
+                        precio: $('#precio').val(),
+                        categoria: $('#categoria').val(),
+                        ubicacion: $('#ubicacion').val()
+                    };
+
+                    try {
+                        const response = await fetch(`http://localhost:8080/inventario/${newId}`, {
+                            method: 'PUT',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(updatedInventario)
+                        });
+                        if (response.ok) {
+                            alert('Elemento actualizado con éxito');
+                            await initDataTable(); // Recargar la tabla después de la actualización
+                        } else {
+                            alert('Error al actualizar el elemento');
+                        }
+                    } catch (error) {
+                        alert('Error al actualizar el elemento');
+                    }
+                });
+            } else {
+                alert('Error al crear la nueva entrada');
+            }
+        } catch (error) {
+            alert('Error al crear la nueva entrada');
+        }
+    });
 });
+
+
